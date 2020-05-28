131715e2ef1e197e542daed83431bbbe02bef2de
Anastasia <anastasia.tsypkaykina@students.fhnw.ch>
2020-05-28 10:46:23 +0200

    Profile: Avatar

--- app/src/main/java/ch/fhnw/ip6/citycourier/ui/profile/ProfileScreen.kt
+++ app/src/main/java/ch/fhnw/ip6/citycourier/ui/profile/ProfileScreen.kt
@@ -1,10 +1,44 @@
 package ch.fhnw.ip6.citycourier.ui.profile
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Clip
+import androidx.ui.layout.Size
+import androidx.ui.core.dp
+import androidx.ui.core.setContent
+import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.CircleShape
+import androidx.ui.layout.Container
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
-import ch.fhnw.ip6.citycourier.ui.welcome.WelcomeScreen
+import ch.fhnw.ip6.citycourier.R
+
+class ProfileScreen : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+                setContent {
+
+        }
+    }
+}
+
+
 @Composable
-fun ProfileScreen(){
+fun Picture(){
+    val image = +imageResource(R.drawable.ic_profile_60)
+    Container(modifier = Size(130.dp, 130.dp)) {
+        Clip(shape= CircleShape){
+            DrawImage(image = image)
+        }
+    }
+}
+
+@Composable
+fun ProfileInfo(){
+
 
 }
 
@@ -12,6 +46,7 @@ fun ProfileScreen(){
 @Preview
 @Composable
 fun DefaultPreview() {
-    ProfileScreen()
+    Picture()
+    ProfileInfo()
 
 }
