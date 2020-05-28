aeb88c9245e16e8a87e05476589fbe5ed1ec7c8b
Anastasia <anastasia.tsypkaykina@students.fhnw.ch>
2020-05-28 12:15:49 +0200

    ProfileScreen edited

--- app/src/main/java/ch/fhnw/ip6/citycourier/ui/profile/ProfileScreen.kt
+++ app/src/main/java/ch/fhnw/ip6/citycourier/ui/profile/ProfileScreen.kt
@@ -1,8 +1,5 @@
 package ch.fhnw.ip6.citycourier.ui.profile
 
-import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.ambient
 import androidx.compose.unaryPlus
@@ -14,28 +11,19 @@ import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.vector.DrawVector
 import androidx.ui.layout.*
 import androidx.ui.layout.Size
-import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.imageResource
 import androidx.ui.res.vectorResource
 import androidx.ui.text.TextStyle
 import androidx.ui.tooling.preview.Preview
 import ch.fhnw.ip6.citycourier.R
-import ch.fhnw.ip6.citycourier.data.dataService
-import ch.fhnw.ip6.citycourier.model.Profile
 import ch.fhnw.ip6.citycourier.ui.LightThemeColors
 import ch.fhnw.ip6.citycourier.ui.themeTypography
-import ch.fhnw.ip6.citycourier.ui.welcome.NotificationList
-
 
 
 @Composable
 fun ProfileScreen() {
-    val context = +ambient(ContextAmbient)
-
-
     MaterialTheme (colors = LightThemeColors, typography = themeTypography) {
-        VerticalScroller(isScrollable = true) {
             Column( modifier = Spacing(5.dp)) {
                 CityCourierLogo()
                 HeightSpacer(5.dp)
@@ -47,12 +35,14 @@ fun ProfileScreen() {
                 Picture()
                 ProfileInfo()
             }
-        }
-
     }
 }
 @Composable
 fun Picture(){
+    Row(modifier = Width(130.dp)){
+        Text(text = "Profile info",style = TextStyle(fontSize = TextUnit.Companion.Sp(28)))
+        }
+    HeightSpacer(height = 15.dp)
     val image = +imageResource(R.drawable.ic_profile_60)
     Container(modifier = Size(130.dp, 130.dp)) {
         Clip(shape= CircleShape){
@@ -63,23 +53,19 @@ fun Picture(){
 
 @Composable
 fun ProfileInfo(){
-    HeightSpacer(height = 10.dp)
-    Text(text="Profile Info", modifier = Width(130.dp), style = TextStyle(fontSize = TextUnit.Companion.Sp(28)))
-    Column(modifier = Width(130.dp)) {
-        Row(modifier = Width(50.dp)) {
-            Text(text="Name", modifier = Width(50.dp))
-            Text(text="First name", modifier = Width(50.dp))
-            Text(text="Contact", modifier = Width(50.dp))
+    HeightSpacer(height=10.dp)
+    Column(modifier=Width(130.dp)){
+        Row(modifier=Width(50.dp)){
+            Text(text="Name",modifier=Width(50.dp))
         }
     }
-    Column(modifier = Width(130.dp)) {
-        Row() {
-            Text(text="Martin", modifier = Width(50.dp))
-            Text(text="Keller", modifier = Width(50.dp))
-            Text(text="martin.keller@citycourier.ch", modifier = Width(50.dp))
+    Column(modifier=Width(130.dp)){
+        Row(){
+            Text(text="Martin",modifier=Width(50.dp))
         }
     }
 }
+
 
 @Composable
 private fun CityCourierLogo() {
@@ -120,7 +106,7 @@ fun IconsOverview() {
                         alignment = Alignment.TopCenter
                     ) {
                         Clip(shape = RoundedCornerShape(8.dp)) {
-                            DrawVector(vectorImage = +vectorResource(R.drawable.Online_Connection))
+                            DrawVector(vectorImage = +vectorResource(R.drawable.online_connection))
                         }
 
                     }
@@ -141,10 +127,8 @@ fun IconsOverview() {
 
 @Preview
 @Composable
-fun DefaultPreview() {
-
+fun ProfilePreview() {
     ProfileScreen()
-
 }
 
 
