7505d475623d6c0bba23fc3ec097dc4acdaa88e7
Anastasia <anastasia.tsypkaykina@students.fhnw.ch>
2020-05-28 13:13:05 +0200

    ProfileScreen edited

--- app/src/main/java/ch/fhnw/ip6/citycourier/ui/profile/ProfileScreen.kt
+++ app/src/main/java/ch/fhnw/ip6/citycourier/ui/profile/ProfileScreen.kt
@@ -25,11 +25,10 @@ import ch.fhnw.ip6.citycourier.ui.themeTypography
 fun ProfileScreen() {
     MaterialTheme (colors = LightThemeColors, typography = themeTypography) {
             Column( modifier = Spacing(5.dp)) {
-                CityCourierLogo()
+                CourierLogo()
                 HeightSpacer(5.dp)
-
                 Row(modifier = Spacing(2.dp), arrangement = Arrangement.Center ) {
-                    IconsOverview()
+                    StatusBar()
                 }
                 HeightSpacer(5.dp)
                 Picture()
@@ -40,13 +39,16 @@ fun ProfileScreen() {
 @Composable
 fun Picture(){
     Row(modifier = Width(130.dp)){
-        Text(text = "Profile info",style = TextStyle(fontSize = TextUnit.Companion.Sp(28)))
-        }
-    HeightSpacer(height = 15.dp)
-    val image = +imageResource(R.drawable.ic_profile_60)
+        Text(text = "Profile info",
+            //style = TextStyle(fontSize = TextUnit.Companion.Sp(28))
+            style = themeTypography.subtitle1
+        )
+    }
+    HeightSpacer(height = 25.dp)
+
     Container(modifier = Size(130.dp, 130.dp)) {
         Clip(shape= CircleShape){
-            DrawImage(image = image)
+            DrawVector(vectorImage = +vectorResource(R.drawable.ic_profile_60))
         }
     }
 }
@@ -54,13 +56,9 @@ fun Picture(){
 @Composable
 fun ProfileInfo(){
     HeightSpacer(height=10.dp)
-    Column(modifier=Width(130.dp)){
-        Row(modifier=Width(50.dp)){
+    Row(modifier = Width(130.dp)) {
+        Column(modifier = Width(65.dp)) {
             Text(text="Name",modifier=Width(50.dp))
-        }
-    }
-    Column(modifier=Width(130.dp)){
-        Row(){
             Text(text="Martin",modifier=Width(50.dp))
         }
     }
@@ -68,7 +66,7 @@ fun ProfileInfo(){
 
 
 @Composable
-private fun CityCourierLogo() {
+fun CourierLogo() {
     Container(modifier = Height(110.dp) wraps Expanded) {
         Clip(shape = RoundedCornerShape(8.dp)) {
             DrawImage(image = +imageResource(R.drawable.deliveryservice_logo))
@@ -77,7 +75,7 @@ private fun CityCourierLogo() {
 }
 
 @Composable
-fun IconsOverview() {
+fun StatusBar() {
     MaterialTheme (colors = LightThemeColors, typography = themeTypography) {
         FlexRow(modifier = Spacing(14.dp)) {
             expanded(flex=1.0f) {
@@ -88,15 +86,15 @@ fun IconsOverview() {
                         alignment = Alignment.TopCenter
                     ) {
                         Clip(shape = RoundedCornerShape(8.dp)) {
-                            DrawVector(vectorImage = +vectorResource(R.drawable.arrow))
+                            DrawImage(image = +imageResource(R.drawable.arrow))
                         }
 
                     }
-                    Text("Orders", style = themeTypography.subtitle1)
+
                 }
             }
 
-            WidthSpacer(width = 40.dp)
+            WidthSpacer(width = 10.dp)
 
             expanded(flex=1.0f){
                 Column(){
@@ -106,11 +104,11 @@ fun IconsOverview() {
                         alignment = Alignment.TopCenter
                     ) {
                         Clip(shape = RoundedCornerShape(8.dp)) {
-                            DrawVector(vectorImage = +vectorResource(R.drawable.online_connection))
+                            DrawImage(image = +imageResource(R.drawable.online_connection))
                         }
 
                     }
-                    Text("Profile" , style= themeTypography.subtitle1 )
+                    Text("Connection" , style= themeTypography.subtitle1 )
 
                 }
             }
